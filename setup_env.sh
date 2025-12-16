cbd224fa044f51d000b419e0d18dad49a66d0668
Fábio Chicout <fchicout@gmail.com>
2025-12-16 16:31:44 -0300

    Sanitize setup_env.sh removing secrets

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export ZOTERO_API_KEY="b01dBXe0ai3xoRzICPfQgILF"
-export ZOTERO_TARGET_GROUP="https://www.zotero.org/groups/6287212/rsl-xm"
+# Example setup_env.sh
+# Replace these values with your own keys before running
+export ZOTERO_API_KEY="your_zotero_api_key_here"
+export ZOTERO_TARGET_GROUP="https://www.zotero.org/groups/1234567/your_group_name"
